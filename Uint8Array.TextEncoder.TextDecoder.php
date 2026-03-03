7d39991e4f4b7b19dfc7395c53b818754985853b
Christian Feilert <65924978+KO81@users.noreply.github.com>
2026-03-03 03:55:08 +0100

    Update Uint8Array.TextEncoder.TextDecoder.php
    removed two unnecessary base conversations at the decoder section. causing pack to not throwing warnings.

--- Uint8Array.TextEncoder.TextDecoder.php
+++ Uint8Array.TextEncoder.TextDecoder.php
@@ -9,8 +9,8 @@ function num($int,$base=2){return base_convert($int,$base,10);
 };
 function str($str,$base=2){return base_convert($str,10,$base);
 };
-function sT($string){$new_str_0='';$new_str_1=str_split($string,8);
-	foreach($new_str_1 as $res){$new_str_0.=pack('H8', base_convert($res,2,16));
+function sT($string){$new_str_0='';$new_str_1=explode(',',$string);
+	foreach($new_str_1 as $res){$new_str_0.=pack('C',intval($res));
 	};return $new_str_0;
 };
 function sB($string){$String='';
@@ -29,27 +29,23 @@ class Uint8Array{
 		}else{$this->array=array_fill(0,$length,0);
 		};$this->buffer=implode(',',$this->array);$this->length=count($this->array);
 	}
-};
-class TextEncoder{
+};class TextEncoder{
 	public $txt,$array=[];
 	function encode($str=''){
 		$this->txt=str_split(sB($str),8);
 		foreach($this->txt as $k => $v)array_push($this->array,num($v));
 		return new Uint8Array($this->array);
 	}
-};
-class TextDecoder{
+};class TextDecoder{
 	public $txt,$array=[],$result;
 	function decode($txt=[]){
 		try{$this->txt=json_decode(json_encode($txt,true),true);
-			foreach($this->txt['array'] as $k => $v)array_push($this->array,str_pad(str($v),8,'0',STR_PAD_LEFT));
-			$this->result=sT(implode('',$this->array));			// more object based
-			//return sT(implode('',$this->array));				// less object based
-		}catch(exception $e){return 'not valid Uint8Array';
+			//$this->result=sT(implode('',$this->array));	// more object based
+			return sT(implode(',',$this->txt));				// less object based
+		}catch(Exception $e){return 'not valid Uint8Array';
 		}
 	}
-};
-class TextEncoderUTF8{
+};class TextEncoderUTF8{
 	public $txt,$array=[],$arr;
 	function __construct(){$this->arr=index();
 	}function encode($txt=''){
@@ -57,8 +53,7 @@ class TextEncoderUTF8{
 		foreach($this->txt as $k => $v)array_push($this->array,array_search($v,$this->arr));
 		return new Uint8Array($this->array);
 	}
-};
-class TextDecoderUTF8{
+};class TextDecoderUTF8{
 	public $txt,$array=[],$arr,$text=[],$result='';
 	function __construct(){$this->arr=index();
 	}function decode($txt=[]){
